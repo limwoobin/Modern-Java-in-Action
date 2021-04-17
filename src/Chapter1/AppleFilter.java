e6c75d4035bd1d251e8dd458e6839dc872de1b3f
limwoobin <drogba02@naver.com>
2021-04-17 18:13:45 +0900

    Chapter1 - 메서드 전달에서 람다로

--- src/Chapter1/AppleFilter.java
+++ src/Chapter1/AppleFilter.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
+import static Chapter1.Color.GREEN;
+
 public class AppleFilter {
     public static List<Apple> filterApples(List<Apple> inventory , Predicate<Apple> p) {
         List<Apple> result = new ArrayList<>();
@@ -18,8 +20,12 @@ public class AppleFilter {
 
     public static void main(String[] args) {
         // 메서드 전달
-        List<Apple> inventory = List.of(new Apple(Color.GREEN , 100) , new Apple(Color.RED , 50));
+        List<Apple> inventory = List.of(new Apple(GREEN , 100) , new Apple(Color.RED , 50));
         List<Apple> greenApples = filterApples(inventory , Apple::isGreenApple);
         List<Apple> heavyApples = filterApples(inventory , Apple::isHeavyApple);
+
+        // 메서드 전달에서 람다로
+        List<Apple> greenApples2 = filterApples(inventory , (Apple a) -> GREEN.equals(a.getColor()));
+        List<Apple> heavyApples2 = filterApples(inventory , (Apple a) -> 150 > a.getWeight());
     }
 }
